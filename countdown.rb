a480e9f38546422c548f6520e5e680757478d704
Fred Murigi <freddiemurigi@gmail.com>
2023-02-09 11:23:12 +0300

    completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+def countdown(num)
+    while num > 0
+        puts "#{num} SECOND(S)!"
+        num -= 1
+        sleep(1)
+    end
+    "HAPPY NEW YEAR!"
 end
+    
+puts countdown(10)
